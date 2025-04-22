f0185c7b00252d3c527bed4de5a5146cfb1983ae
lakshay-0423 <blakshay162@gmail.com>
2025-04-22 14:34:31 +0530

    wallet js 5
    change 5

--- wallet.js
+++ wallet.js
@@ -2,4 +2,5 @@ function addMoney() {
     const amount = prompt("Enter amount to add:");
     if (amount && !isNaN(amount)) {
         const balanceElement = document.getElementById('balance');
+        let currentBalance = parseFloat(balanceElement.innerText);
 }

21e4d096eb69d9aa4c882202c7d94b4aae562bae
Mehak021205 <mehak0453.becse24@chitkara.edu.in>
2025-04-23 11:35:28 +0530

    app.5
    app.5

--- app.js
+++ app.js
@@ -14,3 +14,9 @@ mobileLink.addEventListener("click",function(){
 var step = 100;
 var stepFilter = 60;
 var scrolling = true;
+$(".back").bind("click",function(e){
+    e.preventDefault();
+    $(".highlight-wrapper").animate({
+        scrollLeft: "-=" + step + "px"
+    });
+});
